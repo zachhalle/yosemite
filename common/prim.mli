0a7689a2d30394d3d182c3a5f5a53dd70ce1cc8d
Zach Halle <zach.halle95@gmail.com>
2022-10-01 17:50:40 -0700

    prim

---
+++ common/prim.mli
@@ -0,0 +1,18 @@
+type primitive =
+   | Neg
+   | Plus
+   | Minus
+   | Times
+   | Div
+   | Mod
+   | Eq_int
+   | Lt_int
+   | Leq_int
+   | Int_to_string
+   
+   | Eq_char
+   
+   | Concat
+   | Eq_string
+   | Lt_string
+   | Print
